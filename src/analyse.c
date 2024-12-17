1b552bf20b1c55b5f5636f954c6614361de50f6e
joshhh7 <162483261+joshhh7@users.noreply.github.com>
2024-12-17 19:16:48 +0100

    Add files via upload

--- src/analyse.c
+++ src/analyse.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <malloc.h>
 #include "../include/matrix.h"
 #include "../include/strassen.h"
 #include "../include/LU.h"
@@ -22,7 +23,7 @@ void fillMatrixRandom(matrix *M)
 
 
 
-double tpsMoy_MultNaive(int nbTests, int taille)
+double tpsMoy_MultNaive(int nbTests, int taille, int tpsMem)
 {
     matrix *A = creeMatrix(taille, taille);
     matrix *B = creeMatrix(taille, taille);
@@ -30,26 +31,36 @@ double tpsMoy_MultNaive(int nbTests, int taille)
 
     clock_t start, end;
     double tempsTotal = 0.0;
+    double memTotal = 0.0;
     for (int i=0; i < nbTests; i++)
     {
         fillMatrixRandom(A);
         fillMatrixRandom(B);
+
+        struct mallinfo2 mem_before = mallinfo2();
 
         start = clock();
         naiveMultMat(A, B, C);
         end = clock();
 
+        struct mallinfo2 mem_after = mallinfo2();
+
         tempsTotal += (double)(end - start);
+        memTotal += (double)(mem_after.uordblks - mem_before.uordblks);
     }
     
     freeMatrix(*A);
     freeMatrix(*B);
     freeMatrix(*C);
+    //si tpsMem == 1, on renvoie la quantité de mémoire utilisée
+    if(tpsMem)
+        return (double)(memTotal / nbTests);
+    //on renvoie le temps d'exécution sinon
     return (double)(tempsTotal / nbTests);
 }
 
 
-double tpsMoy_MultStrassen(int nbTests, int taille)
+double tpsMoy_MultStrassen(int nbTests, int taille, int tpsMem, int limite)
 {
     matrix *A = creeMatrix(taille, taille);
     matrix *B = creeMatrix(taille, taille);
@@ -57,21 +68,29 @@ double tpsMoy_MultStrassen(int nbTests, int taille)
 
     clock_t start, end;
     double tempsTotal = 0.0;
+    double memTotal = 0.0;
     for (int i=0; i < nbTests; i++)
     {
         fillMatrixRandom(A);
         fillMatrixRandom(B);
 
+        struct mallinfo2 mem_before = mallinfo2();
+
         start = clock();
-        strassen(A, B, C, 200);
+        strassen(A, B, C, limite);
         end = clock();
 
+        struct mallinfo2 mem_after = mallinfo2();
+
         tempsTotal += (double)(end - start);
+        memTotal += (double)(mem_after.uordblks - mem_before.uordblks);
     }
     
     freeMatrix(*A);
     freeMatrix(*B);
     freeMatrix(*C);
+    if(tpsMem)
+        return (double)(memTotal / nbTests);
     return (double)(tempsTotal / nbTests);
 }
 
@@ -79,97 +98,153 @@ double tpsMoy_MultStrassen(int nbTests, int taille)
 
 
 
-double tpsMoy_InversePLUQ(int nbTests, int taille)
+double tpsMoy_InversePLUQ(int nbTests, int taille, int tpsMem)
 {
     matrix *A = creeMatrix(taille, taille);
     matrix *B = creeMatrix(taille, taille);
 
     clock_t start, end;
     double tempsTotal = 0.0;
+    double memTotal = 0.0;
     for (int i=0; i < nbTests; i++)
     {
         fillMatrixRandom(A);
+
+        struct mallinfo2 mem_before = mallinfo2();
 
         start = clock();
         inverse_PLUQ(A, B);
         end = clock();
 
+        struct mallinfo2 mem_after = mallinfo2();
+
         tempsTotal += (double)(end - start);
+        memTotal += (double)(mem_after.uordblks - mem_before.uordblks);
     }
     
     freeMatrix(*A);
     freeMatrix(*B);
+    if(tpsMem)
+        return (double)(memTotal / nbTests);
     return (double)(tempsTotal / nbTests);
 }
 
 
-double tpsMoy_InverseStrassenNaive(int nbTests, int taille)
+double tpsMoy_InverseStrassenNaive(int nbTests, int taille, int tpsMem)
 {
     matrix *A = creeMatrix(taille, taille);
     matrix *B = creeMatrix(taille, taille);
 
     clock_t start, end;
     double tempsTotal = 0.0;
+    double memTotal = 0.0;
     for (int i=0; i < nbTests; i++)
     {
         fillMatrixRandom(A);
+
+        struct mallinfo2 mem_before = mallinfo2();
 
         start = clock();
         strassen_inverse_recursive_naive(A, B);
         end = clock();
 
+        struct mallinfo2 mem_after = mallinfo2();
+
         tempsTotal += (double)(end - start);
+        memTotal += (double)(mem_after.uordblks - mem_before.uordblks);
     }
     
     freeMatrix(*A);
     freeMatrix(*B);
+    if(tpsMem)
+        return (double)(memTotal / nbTests);
     return (double)(tempsTotal / nbTests);
 }
 
 
-double tpsMoy_InverseStrassenStrassen(int nbTests, int taille)
+double tpsMoy_InverseStrassenStrassen(int nbTests, int taille, int tpsMem, int limite)
 {
     matrix *A = creeMatrix(taille, taille);
     matrix *B = creeMatrix(taille, taille);
 
     clock_t start, end;
     double tempsTotal = 0.0;
+    double memTotal = 0.0;
     for (int i=0; i < nbTests; i++)
     {
         fillMatrixRandom(A);
 
+        struct mallinfo2 mem_before = mallinfo2();
+
         start = clock();
-        strassen_inverse_recursive_strassen(A, B, 200);
+        strassen_inverse_recursive_strassen(A, B, limite);
         end = clock();
 
+        struct mallinfo2 mem_after = mallinfo2();
+
         tempsTotal += (double)(end - start);
+        memTotal += (double)(mem_after.uordblks - mem_before.uordblks);
     }
     
     freeMatrix(*A);
     freeMatrix(*B);
+    if(tpsMem)
+        return (double)(memTotal / nbTests);
     return (double)(tempsTotal / nbTests);
 }
 
 
 
 
-void analysePetitesTailles(double (*func)(int, int), int nbTests, int tailleMax)
+void analysePetitesTailles(double (*func)(int, int, int), int nbTests, int tailleMax, int tpsMem)
 {
-    printf("temps moyen pour chaque taille :\n[");
+    if(tpsMem)
+        printf("mémoire (allocations) utilisée pour chaque taille :\n[");
+    else
+        printf("temps moyen pour chaque taille :\n[");
     for(int i=2; i <= tailleMax; i++)
     {
-        printf("%lf, ", func(nbTests, i));
+        printf("%lf, ", func(nbTests, i, tpsMem));  //matrices de tailles 2 à tailleMax
+    }
+    printf("]\n");
+}
+
+void analysePetitesTaillesStrassen(double (*func)(int, int, int, int), int nbTests, int tailleMax, int tpsMem, int limite)
+{
+    if(tpsMem)
+        printf("mémoire (allocations) utilisée pour chaque taille :\n[");
+    else
+        printf("temps moyen pour chaque taille :\n[");
+    for(int i=2; i <= tailleMax; i++)
+    {
+        printf("%lf, ", func(nbTests, i, tpsMem, limite));  //matrices de tailles 2 à tailleMax
     }
     printf("]\n");
 }
 
 
-void analyseGrandesTailles(double (*func)(int, int), int nbTests, int tailleMax)
+void analyseGrandesTailles(double (*func)(int, int, int), int nbTests, int tailleMax, int tpsMem)
 {
-    printf("temps moyen pour chaque taille :\n[");
-    for(int i=1; i < tailleMax; i++)
+    if(tpsMem)
+        printf("mémoire (allocations) utilisée pour chaque taille :\n[");
+    else
+        printf("temps moyen pour chaque taille :\n[");
+    for(int i=1; i <= tailleMax; i++)
     {
-        printf("%lf, ", func(nbTests, 1<<i));
+        printf("%lf, ", func(nbTests, 1<<i, tpsMem));   //matrices de tailles en puissance de 2
+    }
+    printf("]\n");
+}
+
+void analyseGrandesTaillesStrassen(double (*func)(int, int, int, int), int nbTests, int tailleMax, int tpsMem, int limite)
+{
+    if(tpsMem)
+        printf("mémoire (allocations) utilisée pour chaque taille :\n[");
+    else
+        printf("temps moyen pour chaque taille :\n[");
+    for(int i=1; i <= tailleMax; i++)
+    {
+        printf("%lf, ", func(nbTests, 1<<i, tpsMem, limite));   //matrices de tailles en puissance de 2
     }
     printf("]\n");
 }
--- src/main.c
+++ src/main.c
@@ -12,73 +12,71 @@
 
 int main(int argc, char const *argv[])
 {
-/*    int n =2*2*2*2*2*2*2*2;
-
-    matrix *A = creeMatrix(n, n);
-    matrix *B = creeMatrix(n, n);
-    matrix *C = creeMatrix(n, n);
-
-    matrix *P = creeMatrix(n, n);
-    matrix *L = creeMatrix(n, n);
-    matrix *U = creeMatrix(n, n);
-    matrix *Q = creeMatrix(n, n);
-
-    double M[16] = {
-    0, 1, 3, 1,
-    0, 1, 1, 5,
-    2, 3, 4, 1, 
-    1, 2, 1, 1};
-
-    srand(time(NULL));
-
-    for (int i = 0; i < A->rows; i++)
-    {
-        for (int j = 0; j < A->columns; j++)
-        {
-            A->coefs[i][j] = (float)rand()/((float)RAND_MAX/1);
-        }
-    }
-
-    for (int i = 0; i < B->rows; i++)
-    {
-        for (int j = 0; j < B->columns; j++)
-        {
-            B->coefs[i][j] = rand() % 10;
-        }
-    }
-
-    clock_t start = clock();
-    strassen_inverse_recursive_strassen(A, B, 50);
-    clock_t end = clock();
-
-    double temps = end - start;
-
-    start = clock();
-    strassen_inverse_recursive_naive(A, B);
-    end = clock();
-
-    double temps2 = end - start;
-
-    printf("temps=%lf , %lf\n", temps, temps2);
-
-    naiveMultMat(A, B, P);
-
-    printf("valide = %d\n",testIdentity(P));*/
+    //Tests temps d'exécution
     printf("Multiplication naive petites tailles, ");
-    analysePetitesTailles(tpsMoy_MultNaive, 100, 64);
-    printf("\nMultiplication strassen petites tailles, ");
-    analysePetitesTailles(tpsMoy_MultStrassen, 100, 64);
+    analysePetitesTailles(tpsMoy_MultNaive, 40, 64, 0);
+    printf("\nMultiplication strassen petites tailles Limite=200, ");
+    analysePetitesTaillesStrassen(tpsMoy_MultStrassen, 40, 64, 0, 200);
+    printf("\nMultiplication strassen petites tailles Limite=128, ");
+    analysePetitesTaillesStrassen(tpsMoy_MultStrassen, 40, 64, 0, 128); 
+    printf("\nMultiplication strassen petites tailles Limite=64, ");
+    analysePetitesTaillesStrassen(tpsMoy_MultStrassen, 40, 64, 0, 64);
 
-    printf("\n\nMultiplication naive grandes tailles, ");
-    analyseGrandesTailles(tpsMoy_MultNaive, 30, 11);
-    printf("\nMultiplication strassen grandes tailles, ");
-    analyseGrandesTailles(tpsMoy_MultStrassen, 30, 11);
+    printf("\n\nMultiplication naive, ");
+    analyseGrandesTailles(tpsMoy_MultNaive, 40, 10, 0);
+    printf("\nMultiplication strassen Limite=256, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 0, 256);
+    printf("\nMultiplication strassen Limite=128, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 0, 128);
+    printf("\nMultiplication strassen Limite=64, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 0, 64);
+    printf("\nMultiplication strassen Limite=32, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 0, 32);
+    printf("\nMultiplication strassen Limite=16, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 0, 16);
+    //printf("\nMultiplication strassen Limite=8, ");
+    //analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 11, 0, 8);
 
     printf("\n\nInversion PLUQ, ");
-    analyseGrandesTailles(tpsMoy_InversePLUQ, 30, 11);
+    analyseGrandesTailles(tpsMoy_InversePLUQ, 30, 10, 0);
     printf("\nInversion strassen (avec multiplication naive), ");
-    analyseGrandesTailles(tpsMoy_InverseStrassenNaive, 30, 11);
-    printf("\nInversion strassen (avec multiplication strassen), ");
-    analyseGrandesTailles(tpsMoy_InverseStrassenStrassen, 30, 11);
+    analyseGrandesTailles(tpsMoy_InverseStrassenNaive, 30, 10, 0);
+    printf("\nInversion strassen (avec multiplication strassen) Limite=64, ");
+    analyseGrandesTaillesStrassen(tpsMoy_InverseStrassenStrassen, 30, 10, 0, 64);
+
+
+
+
+    //Tests mémoire
+    printf("Multiplication naive petites tailles, ");
+    analysePetitesTailles(tpsMoy_MultNaive, 40, 64, 1);
+    printf("\nMultiplication strassen petites tailles Limite=200, ");
+    analysePetitesTaillesStrassen(tpsMoy_MultStrassen, 40, 64, 1, 200);
+    printf("\nMultiplication strassen petites tailles Limite=128, ");
+    analysePetitesTaillesStrassen(tpsMoy_MultStrassen, 40, 64, 1, 128); 
+    printf("\nMultiplication strassen petites tailles Limite=64, ");
+    analysePetitesTaillesStrassen(tpsMoy_MultStrassen, 40, 64, 1, 64);
+
+    printf("\n\nMultiplication naive, ");
+    analyseGrandesTailles(tpsMoy_MultNaive, 40, 10, 1);
+    printf("\nMultiplication strassen Limite=256, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 1, 256);
+    printf("\nMultiplication strassen Limite=128, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 1, 128);
+    printf("\nMultiplication strassen Limite=64, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 1, 64);
+    printf("\nMultiplication strassen Limite=32, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 1, 32);
+    printf("\nMultiplication strassen Limite=16, ");
+    analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 10, 1, 16);
+    //printf("\nMultiplication strassen Limite=8, ");
+    //analyseGrandesTaillesStrassen(tpsMoy_MultStrassen, 40, 11, 1, 8);
+
+    printf("\n\nInversion PLUQ, ");
+    analyseGrandesTailles(tpsMoy_InversePLUQ, 40, 10, 1);
+    printf("\nInversion strassen (avec multiplication naive), ");
+    analyseGrandesTailles(tpsMoy_InverseStrassenNaive, 40, 10, 1);
+    printf("\nInversion strassen (avec multiplication strassen) Limite=64, ");
+    analyseGrandesTaillesStrassen(tpsMoy_InverseStrassenStrassen, 40, 10, 1, 64);
     return 0;
 }
