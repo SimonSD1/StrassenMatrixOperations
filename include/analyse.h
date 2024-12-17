6ada52f88b9325dd61ed0ba2e3504e1ccf70a689
joshhh7 <162483261+joshhh7@users.noreply.github.com>
2024-12-17 19:15:49 +0100

    Add files via upload

--- include/analyse.h
+++ include/analyse.h
@@ -8,12 +8,14 @@
 #include "../include/strassen.h"
 
 void fillMatrixRandom(matrix *M);
-double tpsMoy_MultNaive(int nbTests, int taille);
-double tpsMoy_MultStrassen(int nbTests, int taille);
-double tpsMoy_InversePLUQ(int nbTests, int taille);
-double tpsMoy_InverseStrassenNaive(int nbTests, int taille);
-double tpsMoy_InverseStrassenStrassen(int nbTests, int taille);
-void analysePetitesTailles(double (*func)(int, int), int nbTests, int tailleMax);
-void analyseGrandesTailles(double (*func)(int, int), int nbTests, int tailleMax);
+double tpsMoy_MultNaive(int nbTests, int taille, int tpsMem);
+double tpsMoy_MultStrassen(int nbTests, int taille, int tpsMem, int limite);
+double tpsMoy_InversePLUQ(int nbTests, int taille, int tpsMem);
+double tpsMoy_InverseStrassenNaive(int nbTests, int taille, int tpsMem);
+double tpsMoy_InverseStrassenStrassen(int nbTests, int taille, int tpsMem, int limite);
+void analysePetitesTailles(double (*func)(int, int, int), int nbTests, int tailleMax, int tpsMem);
+void analysePetitesTaillesStrassen(double (*func)(int, int, int, int), int nbTests, int tailleMax, int tpsMem, int limite);
+void analyseGrandesTailles(double (*func)(int, int, int), int nbTests, int tailleMax, int tpsMem);
+void analyseGrandesTaillesStrassen(double (*func)(int, int, int, int), int nbTests, int tailleMax, int tpsMem, int limite);  //Permet de fixer la limite
 
 #endif
